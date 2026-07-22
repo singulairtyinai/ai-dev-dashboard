126901cccc129316662f4b9fad5b0ad51ec3ec2d
singulairtyinai <shahrukh.achakzai@gmail.com>
2026-07-22 10:31:32 +0200

    Update site-config.js

--- assets/js/site-config.js
+++ assets/js/site-config.js
@@ -6,5 +6,5 @@ const SITE_CONFIG = {
   branch: 'main',
   sourcesPath: 'data/sources.json',
   workflowFile: 'fetch-data.yml',
-  passwordHashSHA256: 'REPLACE_WITH_YOUR_PASSWORD_HASH',
+  passwordHashSHA256: '74b32ca264cfe68cc3b26692bed7b42280c0f30927792ecfd032520f2c9587d9',
 };
